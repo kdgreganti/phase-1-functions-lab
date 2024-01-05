27638e3422b1fb5be95d010d556b3dc44af5e6b0
Kyle Greganti <kyle.greganti@yahoo.com>
2024-01-05 15:47:39 -0600

    meets requirements

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks(pickUpLocation) {
+    if (pickUpLocation < 42) {
+      return 42 - pickUpLocation;
+    } else {
+      return pickUpLocation - 42;
+    }
+    }
+
+    function distanceFromHqInFeet (pickUpLocation) {
+        return distanceFromHqInBlocks (pickUpLocation) * 264
+    }
+
+    function distanceTravelledInFeet (a, b) {
+        if (b > a) {
+            return ((b-a) * 264);
+        } else {
+            return ((a-b) * 264);
+        }
+    }
+
+    function calculatesFarePrice (start, destination) {
+        let distance = distanceTravelledInFeet (start, destination);
+        if (distance < 400) {
+        return 0;
+    } if (distance >= 400 && distance < 2000) {
+        return ((528 - 400) * .02);
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+} else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+    }
